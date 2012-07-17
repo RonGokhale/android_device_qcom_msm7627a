45e24cc3c6a94739ba2a124aec4b5ba297f1c366
Santosh Sajjan <ssajjan@codeaurora.org>
2012-07-18 01:08:38 +0530

    Changes done for wifi bringup on JB.
    Add wpa_supplicnat.conf for msm7627A.
    
    Change-Id: Ibffc3384d9e1d68a9f814448038e0344f9845da2

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -116,15 +116,15 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
-#ifeq ($(findstring true,$(BOARD_HAS_ATH_WLAN) $(BOARD_HAS_QCOM_WLAN)),true)
-#include $(CLEAR_VARS)
-#LOCAL_MODULE       := wpa_supplicant.conf
-#LOCAL_MODULE_TAGS  := optional
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-#LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
-#include $(BUILD_PREBUILT)
-#endif
+ifeq ($(findstring true,$(BOARD_HAS_ATH_WLAN) $(BOARD_HAS_QCOM_WLAN)),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := wpa_supplicant.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+include $(BUILD_PREBUILT)
+endif
 
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 #include $(CLEAR_VARS)
