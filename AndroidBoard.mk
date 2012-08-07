a3b867e8c82b63458a09aab7f36bcef26b0f783b
Venkat Ram Prakash Kotni <vkotni@codeaurora.org>
2012-08-07 20:33:25 +0530

    device/qcom/msm7627a: Set proper lib path
    Detect type of 8x25 modem from sys file system. If it is 8x25 2.0 modem,
    set rild.libpath to QMI RIL then  if it is dsds start ril-daemon1.
    
    Change-Id: I95eceb5a6a80330654cf01f6f096c301df87c86e

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -117,6 +117,15 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.ril.path.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.composition_type.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
